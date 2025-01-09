d67927457ddec362794877fe6223498aa08e6ebe
notshrirang <shrirangmahajan123@gmail.com>
2025-01-09 23:02:39 +0530

    feat: add llama2

---
+++ llama/llama2/model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+from llama.llama2.config import LlamaConfig
+from llama.llama2.layers import RMSNorm, SelfAttention, FeedForward, EncoderBlock
+from llama.llama2.utils import precompute_theta_pos_frequencies
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, args: LlamaConfig):
+        super().__init__()
+
+        assert args.vocab_size != -1, "Vocab size must be set"
+
+        self.args = args
+        self.vocab_size = args.vocab_size
+        self.n_layers = args.n_layers
+        self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
+
+        self.layers = nn.ModuleList()
+        for layer_id in range(args.n_layers):
+            self.layers.append(EncoderBlock(args))
+
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, self.vocab_size, bias=False)
+
+        self.freqs_complex = precompute_theta_pos_frequencies(
+            self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device=self.args.device)
+
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token at a time can be processed"
+
+        h = self.tok_embeddings(tokens)
+
+        freqs_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex)
+        h = self.norm(h)
+        logits = self.output(h).float()
+        return logits
+
+
+if __name__ == '__main__':
+    args = LlamaConfig()
+    model = Transformer(args)
+    print(f"LLAMA2 loaded with {sum(p.numel() for p in model.parameters()) / 1e6} M parameters")
---
+++ llama/llama2/utils.py
@@ -0,0 +1,34 @@
+import torch
+
+
+def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, theta: float = 10000.0) -> torch.Tensor:
+    assert head_dim % 2 == 0, "head_dim must be divisible by 2"
+
+    theta_numerator = torch.arange(0, head_dim, 2, device=device).float()
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
+    m = torch.arange(seq_len, device=device).float()
+    freqs = torch.outer(m, theta).float()
+    # We can compute complex numbers in polar form c = r * exp(i * m * theta)
+    freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_complex
+
+
+def apply_rotary_embedding(x: torch.Tensor, freqs: torch.Tensor, device) -> torch.Tensor:
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    freqs_complex = freqs.unsqueeze(0).unsqueeze(2)
+    x_rotated = x_complex * freqs_complex
+    x_out = torch.view_as_real(x_rotated)
+    x_out = x_out.reshape(*x.shape)
+    return x_out.type_as(x).to(device)
+
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    else:
+        return (
+            x[:, :, :, None, :]
+            .expand(batch_size, seq_len, n_kv_heads, n_rep, head_dim)
+            .reshape(batch_size, seq_len, n_kv_heads * n_rep, head_dim)
+        )
